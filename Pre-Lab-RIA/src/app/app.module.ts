91b83a4be14f8466d8961b42899b5bde714372e8
Lolellus <81826356+Lolellus@users.noreply.github.com>
2022-04-23 22:29:24 -0300

    componente de clientes

--- Pre-Lab-RIA/src/app/app.module.ts
+++ Pre-Lab-RIA/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { VentaComponent } from './components/venta/venta.component';
 import { ProductoComponent } from './components/producto/producto.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VentaComponent,
-    ProductoComponent
+    ProductoComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
